2626f5f5c1415416574a01ae2c298e95d03c78fc
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-05 06:08:39 -0700

    Using terniary operator

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,12 +1,22 @@
 /** @format */
-
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function handleClick() {
+    setModalIsOpen(true);
+  }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={handleClick}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
